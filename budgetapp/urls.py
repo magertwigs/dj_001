12f27ca014905767113bab7f3f1f46e60853528b
magertwigs <morganahidi@gmail.com>
2024-02-22 13:43:54 +0300

    commit

---
+++ budgetapp/urls.py
@@ -0,0 +1,11 @@
+
+from django.contrib import admin
+from django.urls import path
+from budgetapp import views
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('', views.home, name='homepage'),
+    path('about/', views.about, name='about'),
+    path('form/', views.form, name='form'),
+]
